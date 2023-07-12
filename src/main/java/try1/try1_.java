8b806ef36ea58ac78608ec98c321211def556e9a
haticebersan <bersanhatice1998@gmail.com>
2023-07-12 17:02:31 +0100

    tryforgitagain

---
+++ src/main/java/try1/try1_.java
@@ -0,0 +1,8 @@
+package try1;
+
+public class try1_ {
+    public static void main(String[] args) {
+        String htc = "htcbersan";
+        System.out.println(htc);
+    }
+}
---
+++ src/main/java/try2/try2_.java
@@ -0,0 +1,8 @@
+package try2;
+
+public class try2_ {
+    public static void main(String[] args) {
+        int htc = 24;
+        System.out.println(htc);
+    }
+}
